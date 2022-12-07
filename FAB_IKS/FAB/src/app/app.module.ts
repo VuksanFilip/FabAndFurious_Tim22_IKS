14ea9826752084d9abbd1a1dc70be29ddf31929b
popovbojana <96697478+popovbojana@users.noreply.github.com>
2022-12-07 20:11:32 +0100

    Added login component

--- FAB_IKS/FAB/src/app/app.module.ts
+++ FAB_IKS/FAB/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
