bfcea4327cbe38ec64b45672fba37468cf3f75d9
popovbojana <96697478+popovbojana@users.noreply.github.com>
2022-12-15 00:50:28 +0100

    Profile component

--- FAB_IKS/FAB/src/app/app-routing.module.ts
+++ FAB_IKS/FAB/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { PassengerProfileComponent } from './components/passenger-profile/passenger-profile.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
 const routes: Routes = [
@@ -9,6 +10,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
+  { path:'profile', component: PassengerProfileComponent },
+
 ];
 
 @NgModule({
