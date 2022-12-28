2ab5e406caa4835960401e3fda525bc960eb5fd6
popovbojana <96697478+popovbojana@users.noreply.github.com>
2022-12-28 15:13:15 +0100

    Added map configuration

--- FAB_IKS/FAB/src/app/components/map/map-registered/map-registered.component.ts
+++ FAB_IKS/FAB/src/app/components/map/map-registered/map-registered.component.ts
@@ -1,15 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+import { MapService } from '../map.service';
 
 @Component({
-  selector: 'app-map-registered',
+  selector: 'app-map',
   templateUrl: './map-registered.component.html',
-  styleUrls: ['./map-registered.component.css']
+  styleUrls: ['./map-registered.component.css'],
 })
-export class MapRegisteredComponent implements OnInit {
+export class MapRegisteredComponent implements AfterViewInit {
+  private map: any;
 
-  constructor() { }
+  constructor(private mapService: MapService) {}
 
-  ngOnInit(): void {
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [45.2396, 19.8227],
+      zoom: 13,
+    });
+
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+    tiles.addTo(this.map);
+
+    this.search();
+    this.addMarker();
+    this.registerOnClick();
+    this.route();
   }
 
+  search(): void {
+    this.mapService.search('Strazilovska 19').subscribe({
+      next: (result) => {
+        console.log(result);
+        L.marker([result[0].lat, result[0].lon])
+          .addTo(this.map)
+          .bindPopup('Pozdrav iz Strazilovske 19.')
+          .openPopup();
+      },
+      error: () => {},
+    });
+  }
+
+  registerOnClick(): void {
+    this.map.on('click', (e: any) => {
+      const coord = e.latlng;
+      const lat = coord.lat;
+      const lng = coord.lng;
+      this.mapService.reverseSearch(lat, lng).subscribe((res) => {
+        console.log(res.display_name);
+      });
+      console.log(
+        'You clicked the map at latitude: ' + lat + ' and longitude: ' + lng
+      );
+      const mp = new L.Marker([lat, lng]).addTo(this.map);
+      alert(mp.getLatLng());
+    });
+  }
+
+  route(): void {
+    L.Routing.control({
+      waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
+    }).addTo(this.map);
+  }
+
+  private addMarker(): void {
+    const lat: number = 45.25;
+    const lon: number = 19.8228;
+
+    L.marker([lat, lon])
+      .addTo(this.map)
+      .bindPopup('Trenutno se nalazite ovde.')
+      .openPopup();
+  }
+
+  ngAfterViewInit(): void {
+    let DefaultIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png',
+    });
+
+    L.Marker.prototype.options.icon = DefaultIcon;
+    this.initMap();
+  }
 }
