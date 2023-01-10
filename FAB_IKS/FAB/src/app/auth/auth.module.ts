fdb7a4f4b93f4fc02175c9cd64557bdf59fca81a
popovbojana <96697478+popovbojana@users.noreply.github.com>
2023-01-10 16:03:10 +0100

    Auth module

---
+++ FAB_IKS/FAB/src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { LoginComponent } from './login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/material/material.module';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+//   declarations: [LoginComponent],
+//   imports: [
+//     CommonModule,
+//     RouterModule,
+//     ReactiveFormsModule,
+//     MaterialModule,
+//     FlexLayoutModule,
+//   ],
+})
+export class AuthModule {}
