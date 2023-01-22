29304efd669863573cf7b035e3ec4db7136997d3
popovbojana <96697478+popovbojana@users.noreply.github.com>
2023-01-23 00:40:27 +0100

    Updated history for passenger

--- FAB_IKS/FAB/src/app/components/map/history-passenger/history-passenger.component.ts
+++ FAB_IKS/FAB/src/app/components/map/history-passenger/history-passenger.component.ts
@@ -1,8 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import * as L from 'leaflet';
 import 'leaflet-routing-machine';
-import { MapService } from '../map.service';
 
 @Component({
   selector: 'app-history-passenger',
@@ -10,47 +7,12 @@ import { MapService } from '../map.service';
   styleUrls: ['./history-passenger.component.css'],
 })
 export class HistoryPassengerComponent implements AfterViewInit {
-  private map: any;
-  // markers: Array<any> = [];
-  fromMarker: any;
-  toMarker: any;
 
-  routeForm = new FormGroup({
-    from: new FormControl(),
-    to: new FormControl(),
-  });
-
-  constructor(private mapService: MapService) {}
+  constructor() {}
 
   ngAfterViewInit(): void {
-    let DefaultIcon = L.icon({
-      iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png',
-    });
-
-    L.Marker.prototype.options.icon = DefaultIcon;
-    this.initMap();
+    
   }
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [45.2555317, 19.8264226],
-      zoom: 13,
-    });
-
-    const tiles = L.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      {
-        maxZoom: 18,
-        minZoom: 3,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }
-    );
-    tiles.addTo(this.map);
-
-    // this.search();
-    // this.addMarker();
-    //this.registerOnClick();
-    // this.route();
-  }
+  
 }
