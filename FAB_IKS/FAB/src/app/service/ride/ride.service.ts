ec84f7da56f5aac7270e69bf6aa817ab719e9d22
VuksanFilip <filipvuksan.iphone@gmail.com>
2023-02-01 22:40:24 +0100

    Update ride.service.ts

--- FAB_IKS/FAB/src/app/service/ride/ride.service.ts
+++ FAB_IKS/FAB/src/app/service/ride/ride.service.ts
@@ -4,67 +4,99 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RideService {
   private value$ = new BehaviorSubject<any>({});
   selectedValue$ = this.value$.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   setValue(test: any) {
     this.value$.next(test);
   }
 
-  createNewRide(requestNewRide: any) : Observable<any> {
-    return this.http.post<any>(environment.apiHost + "api/ride", requestNewRide);
+  createNewRide(requestNewRide: any): Observable<any> {
+    return this.http.post<any>(
+      environment.apiHost + 'api/ride',
+      requestNewRide
+    );
   }
 
-  getActiveRideForDriver(driverId: number) : Observable<any> {
-    return this.http.get<any>(environment.apiHost + "api/ride/driver/" + driverId + "/active");
+  getActiveRideForDriver(driverId: number): Observable<any> {
+    return this.http.get<any>(
+      environment.apiHost + 'api/ride/driver/' + driverId + '/active'
+    );
   }
 
-  getActiveRideForPassenger(passengerId: number) : Observable<any> {
-    return this.http.get<any>(environment.apiHost + "api/ride/passenger/" + passengerId + "/active");
+  getActiveRideForPassenger(passengerId: number): Observable<any> {
+    return this.http.get<any>(
+      environment.apiHost + 'api/ride/passenger/' + passengerId + '/active'
+    );
   }
 
-  getRide(id: number) : Observable<any> {
-    return this.http.get<any>(environment.apiHost + "api/ride/" + id);
+  getRide(id: number): Observable<any> {
+    return this.http.get<any>(environment.apiHost + 'api/ride/' + id);
   }
 
-  withdrawRide(id: number) : Observable<any> {
-    return this.http.put<any>(environment.apiHost + "/api/ride/" + id + "/withdraw", {});
+  withdrawRide(id: number): Observable<any> {
+    return this.http.put<any>(
+      environment.apiHost + '/api/ride/' + id + '/withdraw',
+      {}
+    );
   }
 
-  setPanicReason(id: number, requestPanicReason: any) : Observable<any> {
-    return this.http.put<any>(environment.apiHost + "api/ride/" + id + "/panic", requestPanicReason);
+  setPanicReason(id: number, requestPanicReason: any): Observable<any> {
+    return this.http.put<any>(
+      environment.apiHost + 'api/ride/' + id + '/panic',
+      requestPanicReason
+    );
   }
 
-  startRide(id: number) : Observable<any> {
-    return this.http.put<any>(environment.apiHost + "api/ride/" + id + "/start", {});
+  startRide(id: number): Observable<any> {
+    return this.http.put<any>(
+      environment.apiHost + 'api/ride/' + id + '/start',
+      {}
+    );
   }
 
-  acceptRide(id: number) : Observable<any> {
-    return this.http.put<any>(environment.apiHost + "api/ride/" + id + "/accept", {});
+  acceptRide(id: number): Observable<any> {
+    return this.http.put<any>(
+      environment.apiHost + 'api/ride/' + id + '/accept',
+      {}
+    );
   }
 
-  endRide(id: number) : Observable<any> {
-    return this.http.put<any>(environment.apiHost + "api/ride/" + id + "/end", {});
+  endRide(id: number): Observable<any> {
+    return this.http.put<any>(
+      environment.apiHost + 'api/ride/' + id + '/end',
+      {}
+    );
   }
 
-  cancelRide(id: number, requestCancelReason: any) : Observable<any> {
-    return this.http.put<any>(environment.apiHost + "api/ride/" + id + "/cancel", requestCancelReason);
+  cancelRide(id: number, requestCancelReason: any): Observable<any> {
+    return this.http.put<any>(
+      environment.apiHost + 'api/ride/' + id + '/cancel',
+      requestCancelReason
+    );
   }
 
-  createFavoriteRoute(requestNewFavRoute: any) : Observable<any> {
-    return this.http.post<any>(environment.apiHost + "api/ride/favorites", requestNewFavRoute);
+  createFavoriteRoute(requestNewFavRoute: any): Observable<any> {
+    return this.http.post<any>(
+      environment.apiHost + 'api/ride/favorites',
+      requestNewFavRoute
+    );
   }
 
-  getFavoriteRoutes() : Observable<any> {
-    return this.http.get<any>(environment.apiHost + "api/ride/favorites");
+  getFavoriteRoutes(id: number): Observable<any> {
+    return this.http.get<any>(
+      environment.apiHost + 'api/ride/' + id + '/favorites'
+    );
   }
 
-  deleteFavoriteRoute(id: number) : Observable<any> {
-    return this.http.delete<any>(environment.apiHost + "api/ride/favorites/" + id);
+  deleteFavoriteRoute(id: number): Observable<any> {
+    return this.http.delete<any>(
+      environment.apiHost + 'api/ride/favorites/' + id
+    );
   }
 }
