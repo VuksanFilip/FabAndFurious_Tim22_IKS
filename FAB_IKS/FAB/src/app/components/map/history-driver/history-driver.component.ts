d865928d4315c9f5da87752650fb03159f9f48ce
VuksanFilip <filipvuksan.iphone@gmail.com>
2023-02-01 21:48:21 +0100

    Connected with backend

--- FAB_IKS/FAB/src/app/components/map/history-driver/history-driver.component.ts
+++ FAB_IKS/FAB/src/app/components/map/history-driver/history-driver.component.ts
@@ -4,6 +4,9 @@ import { Component, OnInit } from '@angular/core';
 import 'leaflet-routing-machine';
 import { DriverService } from 'src/app/service/driver/driver.service';
 import { DriverRides } from 'src/app/model/Driver';
+import { IdEmail } from 'src/app/model/User';
+import { Path } from 'src/app/model/Location';
+import { ReasonAndTimeOfRejection } from 'src/app/model/Rejection';
 
 @Component({
   selector: 'app-history-driver',
@@ -19,25 +22,77 @@ export class HistoryDriverComponent implements OnInit {
 
   constructor(private driverService: DriverService) {}
 
-  rides: DriverRides;
+  driver: IdEmail = {
+    id: 0,
+    email: '',
+  };
 
-  // rides: DriverRides = {
-  //   totalCount: 0,
-  //   results: RideWithNoStatus[]
-  // };
+  passenger: IdEmail = {
+    id: 0,
+    email: '',
+  };
+
+  passengers: IdEmail[] = [];
+
+  rejection: ReasonAndTimeOfRejection = {
+    reason: '',
+    timeOfRejection: '',
+  };
+
+  departure: Location = {
+    address: '',
+    latitude: 0,
+    longitude: 0,
+  };
+
+  destination: Location = {
+    address: '',
+    latitude: 0,
+    longitude: 0,
+  };
+
+  path: Path = {
+    departure: this.departure,
+    destination: this.destination,
+  };
+
+  locations: Path[] = [];
+
+  ride: RideWithNoStatus = {
+    id: 0,
+    startTime: '',
+    endTime: '',
+    totalCost: 0,
+    driver: this.driver,
+    passengers: this.passengers,
+    estimatedTimeInMinutes: 0,
+    vehicleVehicleName: '',
+    babyTransport: false,
+    petTransport: false,
+    rejection: this.rejection,
+    locations: this.locations,
+    status: '',
+  };
+
+  rides: RideWithNoStatus[] = [];
+
+  allRides: DriverRides = {
+    totalCount: 0,
+    results: this.rides,
+  };
 
   ngOnInit(): void {
-    this.driverService.getDriverRides(5).subscribe((res: DriverRides) => {
-      this.rides = res;
+    this.driverService.getDriverRides(5).subscribe((rides2) => {
+      this.allRides = rides2;
       this.generateSmartTable();
     });
   }
 
   generateSmartTable() {
-    for (let i = 0; i < this.rides.results.length; i += 1) {
+    for (let i = 0; i < this.allRides.results.length; i += 1) {
       this.fillTable({
-        column1: this.rides.results[i],
-        column2: this.rides.results[i + 1],
+        column1: this.allRides.results[i],
+        column2: this.allRides.results[i + 1],
       });
     }
   }
