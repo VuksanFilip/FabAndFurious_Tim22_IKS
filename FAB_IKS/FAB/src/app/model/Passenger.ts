40e5b2b6980fec21a94701b9bf9a811be75be3c7
VuksanFilip <filipvuksan.iphone@gmail.com>
2023-02-01 21:47:07 +0100

    Update Passenger.ts

--- FAB_IKS/FAB/src/app/model/Passenger.ts
+++ FAB_IKS/FAB/src/app/model/Passenger.ts
@@ -1,36 +1,35 @@
-import { Ride, RideWithNoStatus } from "./Ride";
+import { Ride, RideWithNoStatus } from './Ride';
 
-export interface PassengerWithNoId{
-    name: string;
-    surname: string;
-    profilePicture: string;
-    telephoneNumber: string;
-    email: string;
-    address: string;
-    password: string;
+export interface PassengerWithNoId {
+  name: string;
+  surname: string;
+  profilePicture: string;
+  telephoneNumber: string;
+  email: string;
+  address: string;
+  password: string;
 }
 
-export interface PassengerWithNoPassword{
-    id: number;
-    name: string;
-    surname: string;
-    profilePicture: string;
-    telephoneNumber: string;
-    email: string;
-    address: string;
+export interface PassengerWithNoPassword {
+  id: number;
+  name: string;
+  surname: string;
+  profilePicture: string;
+  telephoneNumber: string;
+  email: string;
+  address: string;
 }
 
+export interface PassengerRides {
+  totalCount: number;
+  results: RideWithNoStatus[];
+}
 
-export interface PassengerRides{
-    totalCount: number;
-    results: RideWithNoStatus[];
-
-export interface PassengerUpdate{
-    name: string;
-    surname: string;
-    profilePicture: string;
-    telephoneNumber: string;
-    email: string;
-    address: string;
-
+export interface PassengerUpdate {
+  name: string;
+  surname: string;
+  profilePicture: string;
+  telephoneNumber: string;
+  email: string;
+  address: string;
 }
