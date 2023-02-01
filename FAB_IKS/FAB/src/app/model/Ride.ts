477300d1afaca8bdcebedd6c61c3204ba2528023
VuksanFilip <filipvuksan.iphone@gmail.com>
2023-02-01 22:40:12 +0100

    Update Ride.ts

--- FAB_IKS/FAB/src/app/model/Ride.ts
+++ FAB_IKS/FAB/src/app/model/Ride.ts
@@ -1,51 +1,83 @@
-import { Path } from "./Location";
-import { IdEmail } from "./User";
-import { ReasonAndTimeOfRejection } from "./Rejection"
+import { Path } from './Location';
+import { IdEmail } from './User';
+import { ReasonAndTimeOfRejection } from './Rejection';
 
-export interface Ride{
-    id: number;
-    startTime: string;
-    endTime: string;
-    totalCost: number;
-    driver: IdEmail
-    passengers: IdEmail[];
-    estimatedTimeInMinutes: number;
-    // vehicleVehicleName: ?;
-    babyTransport: boolean;
-    petTransport: boolean;
-    rejection: ReasonAndTimeOfRejection;
-    locations: Path[];
-    status: String;
+// export interface Ride {
+//   id: number;
+//   startTime: string;
+//   endTime: string;
+//   totalCost: number;
+//   driver: IdEmail;
+//   passengers: IdEmail[];
+//   estimatedTimeInMinutes: number;
+//   // vehicleVehicleName: ?;
+//   babyTransport: boolean;
+//   petTransport: boolean;
+//   rejection: ReasonAndTimeOfRejection;
+//   locations: Path[];
+//   status: String;
+// }
+
+export interface RideWithNoStatus {
+  id: number;
+  startTime: string;
+  endTime: string;
+  totalCost: number;
+  driver: IdEmail;
+  passengers: IdEmail[];
+  estimatedTimeInMinutes: number;
+  vehicleVehicleName: string;
+  babyTransport: boolean;
+  petTransport: boolean;
+  rejection: ReasonAndTimeOfRejection;
+  locations: Path[];
+  status: string;
 }
 
-
-
-export interface RideWithNoStatus{
-    id: number;
-    startTime: string;
-    endTime: string;
-    totalCost: number;
-    driver: IdEmail
-    passengers: IdEmail[];
-    estimatedTimeInMinutes: number;
-    vehicleVehicleName: string;
-    babyTransport: boolean;
-    petTransport: boolean;
-    rejection: ReasonAndTimeOfRejection;
-    locations: Path[];
-    status: string;
+export interface Ride {
+  id: number;
+  startTime: string;
+  endTime: string;
+  totalCost: number;
+  driver: IdEmail;
+  passengers: IdEmail[];
+  estimatedTimeInMinutes: number;
+  vehicleVehicleName: string;
+  babyTransport: boolean;
+  petTransport: boolean;
+  rejection: ReasonAndTimeOfRejection;
+  locations: Path[];
+  status: string;
+  sheduledTime: string;
 }
 
-export interface RequestRide{
-    locations: Path[];
-    passengers: IdEmail[];
-    // vehicleVehicleName: ?;
-    babyTransport: boolean;
-    petTransport: boolean;
-    scheduledTime: string;
+export interface RideFavorite {
+  id: number;
+  name: string;
+  startTime: string;
+  endTime: string;
+  totalCost: number;
+  driver: IdEmail;
+  passengers: IdEmail[];
+  estimatedTimeInMinutes: number;
+  vehicleVehicleName: string;
+  babyTransport: boolean;
+  petTransport: boolean;
+  rejection: ReasonAndTimeOfRejection;
+  locations: Path[];
+  status: string;
 }
 
-export interface PageRides{
-    totalCount: number;
-    results: Ride[];
+export interface RequestRide {
+  locations: Path[];
+  passengers: IdEmail[];
+  // vehicleVehicleName: ?;
+  babyTransport: boolean;
+  petTransport: boolean;
+  scheduledTime: string;
+}
+
+export interface PageRides {
+  totalCount: number;
+  results: Ride[];
 }
