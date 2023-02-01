d6f699b9c0a755aec48018ad1560e9e746e0a3c7
VuksanFilip <filipvuksan.iphone@gmail.com>
2023-02-01 16:29:11 +0100

    Connected with backend

--- FAB_IKS/FAB/src/app/components/map/history-driver/history-driver.component.ts
+++ FAB_IKS/FAB/src/app/components/map/history-driver/history-driver.component.ts
@@ -1,7 +1,9 @@
+import { Location } from './../../../model/Location';
 import { Component, OnInit } from '@angular/core';
 import 'leaflet-routing-machine';
 import { DriverService } from 'src/app/service/driver/driver.service';
 import { DriverRides } from 'src/app/model/Driver';
+import { RideWithNoStatus } from 'src/app/model/Ride';
 
 @Component({
   selector: 'app-history-driver',
@@ -9,18 +11,34 @@ import { DriverRides } from 'src/app/model/Driver';
   styleUrls: ['./history-driver.component.css'],
 })
 export class HistoryDriverComponent implements OnInit {
+  taskArray: RidesContent[] = [];
+
+  fillTable(task: RidesContent) {
+    this.taskArray.push(task);
+  }
 
   constructor(private driverService: DriverService) {}
 
-  rides: DriverRides = {
-    totalCount: 0,
-    results: [],
-  }
-
-  ride2depature : String = this.rides.results[0].locations[0].departure.address;
+  rides!: DriverRides;
 
   ngOnInit(): void {
-    this.driverService.getDriverRides(5).subscribe((rides2) => (this.rides = rides2));
-    
+    this.driverService.getDriverRides(5).subscribe((res: DriverRides) => {
+      this.rides = res;
+      this.generateSmartTable();
+    });
+  }
+
+  generateSmartTable() {
+    for (let i = 0; i < this.rides.results.length; i += 1) {
+      this.fillTable({
+        column1: this.rides.results[i],
+        column2: this.rides.results[i + 1],
+      });
+    }
+  }
 }
+
+export interface RidesContent {
+  column1: RideWithNoStatus;
+  column2: RideWithNoStatus;
 }
