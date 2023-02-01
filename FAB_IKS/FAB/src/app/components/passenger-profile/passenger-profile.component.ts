bd263199a38ff5196db1ff37b86b389d6d492790
VuksanFilip <filipvuksan.iphone@gmail.com>
2023-02-01 21:47:18 +0100

    Update passenger-profile.component.ts

--- FAB_IKS/FAB/src/app/components/passenger-profile/passenger-profile.component.ts
+++ FAB_IKS/FAB/src/app/components/passenger-profile/passenger-profile.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { PassengerUpdate } from 'src/app/model/Passenger';
-import { Passenger, PassengerService } from 'src/app/service/passenger/passenger.service';
+import {
+  Passenger,
+  PassengerService,
+} from 'src/app/service/passenger/passenger.service';
 
 @Component({
   selector: 'app-passenger-profile',
   templateUrl: './passenger-profile.component.html',
-  styleUrls: ['./passenger-profile.component.css']
+  styleUrls: ['./passenger-profile.component.css'],
 })
 export class PassengerProfileComponent implements OnInit {
   passenger: Passenger = {
@@ -17,11 +20,13 @@ export class PassengerProfileComponent implements OnInit {
     email: '',
     address: '',
     password: '',
-  }
+  };
 
-  constructor(private passengerService: PassengerService) { }
+  constructor(private passengerService: PassengerService) {}
 
   ngOnInit(): void {
-    this.passengerService.getPassenger(2).subscribe((passenger2) => (this.passenger = passenger2));
+    this.passengerService
+      .getPassenger(2)
+      .subscribe((passenger2) => (this.passenger = passenger2));
   }
-
+}
