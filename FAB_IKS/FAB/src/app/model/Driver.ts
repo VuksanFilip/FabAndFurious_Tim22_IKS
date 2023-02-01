508a4148e79de634df3f069d16974012577f4847
VuksanFilip <filipvuksan.iphone@gmail.com>
2023-02-01 15:41:02 +0100

    Update module

--- FAB_IKS/FAB/src/app/model/Driver.ts
+++ FAB_IKS/FAB/src/app/model/Driver.ts
@@ -1,41 +1,41 @@
-import { RideWithNoStatus } from "./Ride";
+import { Document } from './Document';
+import { RideWithNoStatus } from './Ride';
 
-export interface DriverWithNoId{
-    name: string;
-    surname: string;
-    profilePicture: string;
-    telephoneNumber: string;
-    email: string;
-    address: string;
-    password: string;
+export interface DriverWithNoId {
+  name: string;
+  surname: string;
+  profilePicture: string;
+  telephoneNumber: string;
+  email: string;
+  address: string;
+  password: string;
 }
 
-export interface DriverWithNoPassword{
-    id: number;
-    name: string;
-    surname: string;
-    profilePicture: string;
-    telephoneNumber: string;
-    email: string;
-    address: string;
+export interface DriverWithNoPassword {
+  id: number;
+  name: string;
+  surname: string;
+  profilePicture: string;
+  telephoneNumber: string;
+  email: string;
+  address: string;
 }
 
-export interface WorkingHour{
-    id: number;
-    start: string;
-    end: string;
+export interface WorkingHour {
+  id: number;
+  start: string;
+  end: string;
 }
 
-export interface StartingWorkingHour{
-    start: string;
+export interface StartingWorkingHour {
+  start: string;
 }
 
-export interface EndingWorkingHour{
-    end: string;
+export interface EndingWorkingHour {
+  end: string;
 }
 
-export interface DriverRides{
-    totalCount: number;
-    results: RideWithNoStatus[];
+export interface DriverRides {
+  totalCount: number;
+  results: RideWithNoStatus[];
 }
-
