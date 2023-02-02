5875062b8d55d48cfedf10f2c3c56a50312911f5
popovbojana <popov.bojana@gmail.com>
2023-02-02 15:29:39 +0100

    Connected with backend

---
+++ FAB_IKS/FAB/src/app/service/admin/admin.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  private value$ = new BehaviorSubject<any>({});
+  selectedValue$ = this.value$.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  setValue(test: any) {
+    this.value$.next(test);
+  }
+
+  getAdmin() : Observable<any>{
+    return this.http.get<any>(environment.apiHost + "api/admin");
+  }
+}
