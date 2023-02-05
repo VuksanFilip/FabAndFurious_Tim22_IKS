6d6ee37d95075517a918249f2f7fbab0cfa1db95
andreakatzenberger <andrea.katzenberger01@gmail.com>
2023-02-06 00:17:17 +0100

    Added change password option

--- FAB_IKS/FAB/src/app/components/driver-profile/driver-profile.component.ts
+++ FAB_IKS/FAB/src/app/components/driver-profile/driver-profile.component.ts
@@ -4,11 +4,14 @@ import { DriverProfile } from 'src/app/model/Driver';
 import { VehicleDriverProfile } from 'src/app/model/Vehicle';
 import { Location } from 'src/app/model/Location';
 import { TokenService } from 'src/app/auth/token/token.service';
+import { UserService } from 'src/app/service/user/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NewOldPassword } from 'src/app/model/User';
 
 @Component({
   selector: 'app-driver-profile',
   templateUrl: './driver-profile.component.html',
-  styleUrls: ['./driver-profile.component.css']
+  styleUrls: ['./driver-profile.component.css'],
 })
 export class DriverProfileComponent implements OnInit {
   driver: DriverProfile = {
@@ -17,13 +20,18 @@ export class DriverProfileComponent implements OnInit {
     telephoneNumber: '',
     email: '',
     address: '',
-  }
-  
+  };
+
+  changepassword: NewOldPassword = {
+    newPassword: '123',
+    oldPassword: '',
+  };
+
   location: Location = {
     address: '',
     latitude: 0,
     longitude: 0,
-  }
+  };
 
   vehicle: VehicleDriverProfile = {
     id: 0,
@@ -35,16 +43,64 @@ export class DriverProfileComponent implements OnInit {
     passengerSeats: 0,
     babyTransport: false,
     petTransport: false,
-  }
+  };
 
-  constructor(private driverService: DriverService, private tokenDecoder: TokenService) { }
+  constructor(
+    private driverService: DriverService,
+    private tokenDecoder: TokenService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     const tokenInfo = this.tokenDecoder.getDecodeAccessToken();
-    this.driverService.getDriver(tokenInfo.id).subscribe((driverProfile) => (this.driver = driverProfile));
+    this.driverService
+      .getDriver(tokenInfo.id)
+      .subscribe((driverProfile) => (this.driver = driverProfile));
     console.log(tokenInfo.role);
 
     // this.driverService.getDriverVehicle(5).subscribe((vehicleProfile) => (this.vehicle = vehicleProfile));
   }
 
+  changePasswordForm = new FormGroup({
+    old: new FormControl('', Validators.required),
+    new: new FormControl('', Validators.required),
+  });
+
+  changePassword() {
+    const newOldPassword: NewOldPassword = {
+      oldPassword: this.changePasswordForm.value.old!,
+      newPassword: this.changePasswordForm.value.new!,
+    };
+    //console.log(newOldPassword);
+    var btn = document.getElementById('change');
+    const tokenInfo = this.tokenDecoder.getDecodeAccessToken();
+    if (this.changePasswordForm.valid) {
+      this.userService
+        .changePassword(tokenInfo.id, newOldPassword)
+        .subscribe((changepassword2) => {
+          this.changepassword = changepassword2;
+          btn!.innerText = 'Password changed';
+          console.log(this.changepassword);
+        });
+    } else {
+      alert('Empty input field');
+      btn!.innerText = 'Change password';
+    }
+    const form = document.getElementById('form');
+    form!.style.display = 'none';
+  }
+
+  show(): void {
+    const form = document.getElementById('form');
+    if (form != null) {
+      var btn = document.getElementById('change');
+      if (form.style.display === 'none') {
+        form.style.display = 'block';
+        btn!.innerText = 'Change password';
+      } else {
+        form.style.display = 'none';
+        btn!.innerText = 'Change password';
+      }
+    }
+  }
 }
