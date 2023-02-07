cd35d30442477ce39af44208427515a96addc205
andreakatzenberger <andrea.katzenberger01@gmail.com>
2023-02-07 13:33:25 +0100

    Revert "Merge branch 'main' of https://github.com/VuksanFilip/FabAndFurious_Tim22_IKS"
    This reverts commit a795efe6e3749a4e5515f849171394fbc374f24e, reversing
    changes made to 1240b205e365cf3a14227012868a5e6250be0c08.

--- FAB_IKS/FAB/src/app/components/passenger-profile/passenger-profile.component.ts
+++ FAB_IKS/FAB/src/app/components/passenger-profile/passenger-profile.component.ts
@@ -1,12 +1,9 @@
-import { UserService } from './../../service/user/user.service';
-import { NewOldPassword } from './../../model/User';
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/auth/token/token.service';
 import {
   Passenger,
   PassengerService,
 } from 'src/app/service/passenger/passenger.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-passenger-profile',
@@ -24,64 +21,12 @@ export class PassengerProfileComponent implements OnInit {
     password: '',
   };
 
-  changepassword: NewOldPassword = {
-    newPassword: '123',
-    oldPassword: '',
-  };
-
-  constructor(
-    private passengerService: PassengerService,
-    private tokenDecoder: TokenService,
-    private userService: UserService
-  ) {}
+  constructor(private passengerService: PassengerService, private tokenDecoder: TokenService) {}
 
   ngOnInit(): void {
     const tokenInfo = this.tokenDecoder.getDecodeAccessToken();
     this.passengerService
       .getPassenger(tokenInfo.id)
       .subscribe((passenger2) => (this.passenger = passenger2));
-  }
-
-  changePasswordForm = new FormGroup({
-    old: new FormControl('', Validators.required),
-    new: new FormControl('', Validators.required),
-  });
-
-  changePassword() {
-    const newOldPassword: NewOldPassword = {
-      oldPassword: this.changePasswordForm.value.old!,
-      newPassword: this.changePasswordForm.value.new!,
-    };
-    //console.log(newOldPassword);
-    var btn = document.getElementById('change');
-    const tokenInfo = this.tokenDecoder.getDecodeAccessToken();
-    if (this.changePasswordForm.valid) {
-      this.userService
-        .changePassword(tokenInfo.id, newOldPassword)
-        .subscribe((changepassword2) => {
-          this.changepassword = changepassword2;
-          btn!.innerText = 'Password changed';
-          console.log(this.changepassword);
-        });
-    } else {
-      alert('Empty input field');
-      btn!.innerText = 'Change password';
-    }
-    const form = document.getElementById('form');
-    form!.style.display = 'none';
-  }
-
-  show(): void {
-    const form = document.getElementById('form');
-    if (form != null) {
-      var btn = document.getElementById('change');
-      if (form.style.display === 'none') {
-        form.style.display = 'block';
-        btn!.innerText = 'Change password';
-      } else {
-        form.style.display = 'none';
-        btn!.innerText = 'Change password';
-      }
-    }
   }
 }
--- FAB_IKS/FAB/src/app/model/User.ts
+++ FAB_IKS/FAB/src/app/model/User.ts
@@ -1,62 +1,63 @@
-export interface User {
-  id?: number;
-  name?: string;
-  surname?: string;
-  profilePicture?: string;
-  telephoneNumber?: string;
-  email: string;
-  address: string;
-  password?: string;
-  blocked?: boolean;
-  active?: boolean;
-  role?: string;
+export interface User{
+    id?: number;
+    name?: string;
+    surname?: string;
+    profilePicture?: string;
+    telephoneNumber?: string;
+    email: string;
+    address: string;
+    password?: string;
+    blocked?: boolean;
+    active?: boolean;
+    role?: string;
 }
 
-export interface UserWithNoPassword {
-  id: number;
-  name: string;
-  surname: string;
-  profilePicture: string;
-  telephoneNumber: string;
-  email: string;
-  address: string;
+export interface UserWithNoPassword{
+    id: number;
+    name: string;
+    surname: string;
+    profilePicture: string;
+    telephoneNumber: string;
+    email: string;
+    address: string;
 }
 
-export interface UserWithNoId {
-  name: string;
-  surname: string;
-  profilePicture: string;
-  telephoneNumber: string;
-  email: string;
-  address: string;
-  password: string;
+export interface UserWithNoId{
+    name: string;
+    surname: string;
+    profilePicture: string;
+    telephoneNumber: string;
+    email: string;
+    address: string;
+    password: string; 
 }
 
-export interface UserPanicResponse {
-  name: string;
-  surname: string;
-  profilePicture: string;
-  telephoneNumber: string;
-  email: string;
-  address: string;
+export interface UserPanicResponse{
+    name: string;
+    surname: string;
+    profilePicture: string;
+    telephoneNumber: string;
+    email: string;
+    address: string;
 }
 
-export interface PageUsers {
-  totalCount: number;
-  results: User[];
+export interface PageUsers{
+    totalCount: number;
+    results: User[];
 }
 
-export interface NewOldPassword {
-  newPassword: string;
-  oldPassword: string;
+export interface NewOldPassword{
+    newPassword: string;
+    oldPassword: string;
 }
 
-export interface NewPasswordCode {
-  newPassword: string;
-  code: string;
+export interface NewPasswordCode{
+    newPassword: string;
+    code: string;
 }
 
-export interface IdEmail {
-  id: number;
-  email: string;
+export interface IdEmail{
+    id: number;
+    email: string;
 }
+
